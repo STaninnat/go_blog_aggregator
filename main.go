debfad993c80835c240f3a13024e86e2cf6eca55
STaninnat <taninnat.s@gmailcom>
2024-09-12 01:21:54 +0700

    write function that can fetch data from a feed and write a worker that fetches feeds continuously

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 
@@ -42,23 +43,27 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
-	mux.HandleFunc("GET /v1/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+	mux.HandleFunc("POST v1/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("GET v1/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
 
-	mux.HandleFunc("POST /v1/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedCreate))
-	mux.HandleFunc("GET /v1/feeds", apiCfg.handlerFeedsGet)
+	mux.HandleFunc("POST v1/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedCreate))
+	mux.HandleFunc("GET v1/feeds", apiCfg.handlerFeedsGet)
 
-	mux.HandleFunc("GET /v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet))
-	mux.HandleFunc("POST /v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowCreate))
-	mux.HandleFunc("DELETE /v1/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowDelete))
+	mux.HandleFunc("GET v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet))
+	mux.HandleFunc("POST v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowCreate))
+	mux.HandleFunc("DELETE v1/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowDelete))
 
-	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
-	mux.HandleFunc("GET /v1/err", handlerErr)
+	mux.HandleFunc("GET v1/healthz", handlerReadiness)
+	mux.HandleFunc("GET v1/err", handlerErr)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	const collectionConcurrency = 10
+	const collectionInterval = time.Minute
+	go startScraping(dbQueries, collectionConcurrency, collectionInterval)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
